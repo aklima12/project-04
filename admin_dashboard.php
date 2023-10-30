761cad8955db148f0a0b9356ee70582d2221bccf
aklima12 <128598790+aklima12@users.noreply.github.com>
2023-10-29 22:27:01 -0600

    Add files via upload

---
+++ admin_dashboard.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="dashboard-style.css">
+</head>
+<body>
+    <div class="manubar">
+        <div class="row">
+            <div class="col">
+               <img src="brand-logo.png" alt="">
+            </div>
+
+            <div class="col-1">
+                <div class="icon">
+                <i class="fa fa-bell"></i>
+               
+
+
+                
+                    <img src="585e4bc4cb11b227491c3395.png" alt="">
+                    
+
+
+                    <h2>aklima</h2>
+                   
+                    <ul>
+                        <li><a href=""><i class="fa fa-sort-desc"></i></a>
+                    
+                            <ul>
+                                <li><a href="">Admin</a></li>
+                                <li><a href="">Teacher</a></li>
+                                <li><a href="">Student</a></li>
+                            </ul>
+                    </li>
+                    </ul>
+
+                    </div>
+                           
+
+
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+
+    <div class="nab-bar">
+
+        <ul>
+            <li><a href=""> <i class="fa -solid fa-dashboard"></i>Dashboard</a></li>
+            <li><a href=""> <i class="fa fa-user-plus"></i> Admission</a></li>
+            <li><a href=""> <i class="fa fa-users"></i> Teachers</a></li>
+            <li><a href=""> <i class="fa fa-users"></i> Students</a></li>
+            <li><a href=""> <i class="fa fa-bars"></i> Students Account</a></li>
+            <li><a href=""> <i class="fa fa-youtube"></i> Playlist</a></li>
+            <li><a href=""> <i class="fa fa-user-plus"></i> Visitor</a></li>
+            <li><a href=""> <i class="fa fa-user-plus"></i> Seminar</a></li>
+            <li><a href=""> <i class="fa fa-id-card"></i> Payment</a></li>
+            <li><a href=""> <i class="fa fa-registered"></i> Registration</a></li>
+            <li><a href=""> <i class="fa fa-comment"></i> Bulk SMS</a></li>
+            <li><a href=""> <i class="fa fa-refresh"></i> Results</a></li>
+            <li><a href=""> <i class="fa fa-bars"></i> Notice</a></li>
+            <li><a href=""> <i class="fa fa-link"></i> File upload</a></li>
+            <li><a href=""> <i class="fa fa-history"></i> Routine</a></li>
+            <li><a href=""> <i class="fa fa-plus"></i> Course</a></li>
+            <li><a href=""> <i class="fa fa-id-card"></i> ID CARD</a></li>
+            <li><a href=""> <i class="fa fa-clock-o""></i> Attendence</a></li>
+
+        </ul>
+
+
+
+
+    </div>
+    
+</body>
+</html>
---
+++ admin_dbcon.php
@@ -0,0 +1,8 @@
+<?php 
+$hostname = "localhost";
+$usernamex = "root";
+$password = "";
+$database_name = "register";
+$admin_dbcon=mysqli_connect($hostname , $usernamex , $password , $database_name);
+
+?>
